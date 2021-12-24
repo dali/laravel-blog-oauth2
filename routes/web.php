4244091c7584df3033dd6229a6f5e9db80ae947c
dali ben rhouma <benrhouma84@gmail.com>
2021-12-24 13:18:01 +0100

    create auth, post model and migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\SocialiteController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +30,17 @@ require __DIR__.'/auth.php';
 Route::get('/sign-in/github', [SocialiteController::class, 'github']);
 
 Route::get('/sign-in/github/redirect', [SocialiteController::class, 'githubRedirect']);
+
+Route::group(['prefix' => 'admin'], function() {
+    
+    Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('posts/new', [PostController::class, 'create']);
+    Route::get('posts/check_slug', [PostController::class, 'checkSlug'])->name('posts.checkSlug');
+    Route::post('posts/store', [PostController::class, 'store'])->name('posts.new');
+    Route::get('posts/show', [PostController::class, 'show']);
+});
+
+Route::resource('posts', \App\Http\Controllers\PostController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
